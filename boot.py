ef2ea9c2d929c1cdca99751b4be61990fddacf66
Noel <noele952@gmail.com>
2023-11-24 11:02:00 -0800

    code comments

--- boot.py
+++ boot.py
@@ -1,78 +1,191 @@
 import network
+import ssd1306
 from classes.config import ConfigManager
-from classes.display import Display
-from classes.mqtt import MQTT
-from classes.garden import Garden
-from classes.menu_items import Menu, Wizard, Selection
+from machine import I2C, Pin
+from rotary_irq_pico import RotaryIRQ
+from umqtt.simple import MQTTClient
+import utime as time
+import sys
+import gc
+
+# Display available memory before any allocations
+print("Free Memory1:", gc.mem_free())
+
+# List of characters for rotary entry and buttons 'back' and 'save'
+my_data = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ") + ['back', 'save']
+
+# Load configuration from file
+app_config = ConfigManager(filename='config.bin')
+print(f'app_config.data: {app_config.data}')
+
+# Initialize I2C for OLED display
+i2c = I2C(app_config.I2C_CHANNEL, sda=Pin(app_config.I2C_DATA), scl=Pin(app_config.I2C_CLOCK))
+
+# Initialize SSD1306 OLED display
+oled = ssd1306.SSD1306_I2C(app_config.OLED_WIDTH,app_config.OLED_HEIGHT, i2c)
 
 
-I2C_CLOCK = 7
-I2C_DATA = 6
-I2C_CHANNEL = 1
-APUMP_PIN = 22  
-WPUMP_PIN = 27  
-HEATER_PIN = 28
-LED_PIN = 15
-THERM_PIN = 26
-OLED_WIDTH = 128
-OLED_HEIGHT = 64
-R_BTN_PIN = 12
-R_CL_PIN = 11
-R_DT_PIN = 10
+# Setup rotary encoder button and knob
+rotary_button = Pin(app_config.R_BTN_PIN, Pin.IN, Pin.PULL_UP)
+rotary_knob = RotaryIRQ(app_config.R_CL_PIN,
+                        app_config.R_DT_PIN,
+                        reverse=False, min_val=0,
+                        max_val=len(my_data)-1,
+                        incr=1,
+                        range_mode=RotaryIRQ.RANGE_WRAP,
+                        pull_up=True,
+                        half_step=False)
 
-AES_CIPHER = b'1234567890123456'
-GARDEN_CONFIG_FILE = 'garden.json'
-MACHINE_ID = 'machine_id'
-CERT_FILE = '/certs/certificate.der'
-KEY_FILE = '/certs/private.der'
+# Display available memory after OLED and rotary setup
+print("Free Memory2:", gc.mem_free())
 
-
-wifi_cred = ConfigManager(filename='config.bin', aes_cipher=AES_CIPHER)
-
-
+# Connect to Wi-Fi
 def do_connect(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(ssid, password)
+        display_text("Connecting", "to network...")
         while not sta_if.isconnected():
             pass
     network_config =sta_if.ifconfig()
-    wifi_cred.save_to_config(network_config[0] + ':5000', 'wifi', 'webapp_address')
+    app_config.save_to_config(network_config[0] + ':5000', 'wifi', 'webapp_address')
     print('Connected! Network config:', network_config)
+    display_text("Connected!", "Network config:", str(network_config[0]))
 
 
+# Log in to Wi-Fi
 def login_wifi():
-    ssid = wifi_cred.get_from_config('wifi', 'credentials', 'ssid')
-    password = wifi_cred.get_from_config('wifi', 'credentials', 'password')
+    ssid = app_config.get_from_config('wifi', 'credentials', 'ssid')
+    password = app_config.get_from_config('wifi', 'credentials', 'password')
+    
     if ssid and password:
         # Connect to Wi-Fi using ssid and password
         print("Connecting to Wi-Fi...")
+        display_text("Connecting", "to WiFi")
         do_connect(ssid, password)
     else:
         # Display login screen and get credentials from the user
         print("Display login screen...")
-        ssid = input("Enter SSID: ")
-        password = input("Enter password: ")
+#         ssid = input("Enter SSID: ")
+#         password = input("Enter password: ")
+        ssid = rotary_entry('SSID')
+        password = rotary_entry('Password')
         # Save credentials and IP for future use
-        wifi_cred.save_to_config(ssid, 'wifi', 'credentials', 'ssid')
-        wifi_cred.save_to_config(password, 'wifi', 'credentials', 'password')
         print("Connecting to Wi-Fi...")
+        display_text("Connecting", "to WiFi")
+        time.sleep(1)
         do_connect(ssid, password)
+        app_config.save_to_config(ssid, 'wifi', 'credentials', 'ssid')
+        app_config.save_to_config(password, 'wifi', 'credentials', 'password')
 
-    
+
+# Display text on OLED
+def display_text(line1='', line2='', line3='', line4=''):
+    oled.fill(0)
+    oled.text(line1, 0, 0)
+    oled.text(line2, 0, 16)
+    oled.text(line3, 0, 32)
+    oled.text(line4, 0, 48)
+    oled.show()
+
+# Function for rotary encoder input entry
+def rotary_entry(value):
+    display_text(f'Enter {value}', 'Turn to Select', 'Push to Confirm')
+    entry = ''
+    val_old = rotary_knob.value()
+    but_old = rotary_button.value()
+    active=True
+    while active:
+        val_new = rotary_knob.value()
+        but_new = rotary_button.value()
+       
+        if but_old == 1 and but_new == 0:  # Check for falling edge (button press)
+            print(f'Button Pressed: {but_new}')
+            if my_data[val_new] == 'back':
+                try:
+                    entry = entry[:-1]
+                except:
+                    entry = ''
+            elif my_data[val_new] == 'save':
+                
+                return entry
+            else:    
+                entry = entry + my_data[val_new]
+            time.sleep(1)
+
+        if val_old != val_new:
+            val_old = val_new
+            display_text(f'Enter {value}', 'Turn to Select', 'Push to Confirm', f'{entry}{my_data[val_new]}') 
+        but_old = but_new
+        time.sleep(0.1)
+        
+# Initial connection to Wi-Fi        
 print("Connecting to your wifi...")
-login_wifi()
+display_text("Connecting", "to your wifi...")
+time.sleep(2)
+login_wifi()       
+gc.collect()
+del my_data
+del rotary_button
+del rotary_knob
 
-# AWS endpoint parameters.
-# Should be different for each device can be anything
-CLIENT_ID="hydro_pico"
-# You can get tihs address from AWS IoT->Settings -> Endpoint
-# mothing like : {host id}.iot.{region}.amazonaws.com
-MQTT_ENDPOINT='mqtt_endpoint'
-garden = Garden(plant_type=None, config_file=GARDEN_CONFIG_FILE, machine_id=MACHINE_ID, waterpump_pin=WPUMP_PIN, airpump_pin=APUMP_PIN,
-                thermistor_pin=THERM_PIN, led_pin=LED_PIN, I2C_channel=I2C_CHANNEL, I2C_clock=I2C_CLOCK, I2C_data=I2C_DATA)
+# Display available memory after Wi-Fi setup
+print("Free Memory3:", gc.mem_free())
 
-mqtt = MQTT(garden, endpoint=MQTT_ENDPOINT, client_id=CLIENT_ID, topic_pub=b'GardenData',
-            cert_file=CERT_FILE, key_file=KEY_FILE)
+# Function for connecting to MQTT broker
+def mqtt_connect(cert_file, key_file, endpoint, client_id):
+    port = 8883
+    keepalive = 3600
+    max_retries = 3
+    retry_count = 0
+    with open(cert_file, 'rb') as f:
+        cert = f.read()
+        
+    with open(key_file, 'rb') as f:
+        key = f.read()
+
+    print("Key and Certificate files Loaded")
+    display_text("MQTT", "Key and", "Certificate", "files loaded")
+    SSL_PARAMS = {'key': key, 'cert': cert, 'server_side': False}
+    while retry_count < max_retries:
+        try:
+            client = MQTTClient(client_id, endpoint, port=port, keepalive=keepalive, ssl=True, ssl_params=SSL_PARAMS)
+            client.connect()
+            print('Connected to %s MQTT Broker' % (endpoint))
+            display_text('', 'Connected to', 'MQTT Broker')
+            return client
+        except OSError as e:
+            print(e)
+            print('Failed to connect to the MQTT Broker. Retrying...')
+            display_text('Failed to connect', 'to MQTT Broker', 'Retrying...')
+            retry_count += 1
+            time.sleep(5)
+
+    print('Failed to connect to the MQTT Broker after multiple attempts.')
+    display_text('Failed to connect', 'to MQTT Broker', f'after {max_retries} attempts')
+    return None
+
+# Connect to MQTT bro
+mqtt_client = mqtt_connect(endpoint=app_config.MQTT_ENDPOINT,
+                           client_id=app_config.MQTT_CLIENT_ID, 
+                           cert_file=app_config.CERT_FILE,
+                           key_file=app_config.KEY_FILE)
+gc.collect()
+time.sleep(2)
+display_text()
+
+print("Free Memory:", gc.mem_free())
+
+# Importing modules and classes
+import uasyncio as asyncio
+
+
+gc.collect()
+
+print("Free Memory5:", gc.mem_free())
+del oled
+del i2c
+
+gc.collect()
+
+print("Free Memory6:", gc.mem_free())
--- main.py
+++ main.py
@@ -1,73 +1,97 @@
-import uasyncio as asyncio
-import gc
-gc.collect()
 from microdot_asyncio import Microdot, Response
 from routes import setup_routes
-from classes.display import Display
-from hydropico_machine.classes.mqtt import MQTT
 from classes.menu_items import Wizard, Menu, Selection
+from classes.display import Display
+from classes.mqtt import MQTT
+from classes.garden import Garden
 from camera import Camera
 from machine import SPI, Pin
-from utime import sleep_ms
-import utime
+from machine import WDT
+from utime import sleep, sleep_ms
 import os
-import sys
 import ubinascii
 import ujson
 
-display = Display(garden, I2C_channel=I2C_CHANNEL, I2C_clock=I2C_CLOCK, I2C_data=I2C_DATA, oled_height=OLED_HEIGHT, oled_width=OLED_WIDTH, rotary_btn_pin=R_BTN_PIN,
-                 rotary_clock_pin=R_CL_PIN, rotary_data_pin=R_DT_PIN)
 
 
+garden = Garden(plant_type=None, config_file=app_config.GARDEN_CONFIG_FILE, machine_id=app_config.MACHINE_ID,
+                waterpump_pin=app_config.WPUMP_PIN, airpump_pin=app_config.APUMP_PIN, thermistor_pin=app_config.THERM_PIN,
+                heater_pin=app_config.HEATER_PIN, photoresistor_pin=app_config.PHOTORES_PIN ,led_pin=app_config.LED_PIN,
+                I2C_channel=app_config.I2C_CHANNEL, I2C_clock=app_config.I2C_CLOCK,
+                I2C_data=app_config.I2C_DATA)
+
+gc.collect()
+print("Free Memory7:", gc.mem_free())
+
+# Create an instance of the Display class
+display = Display(garden, I2C_channel=app_config.I2C_CHANNEL, I2C_clock=app_config.I2C_CLOCK, I2C_data=app_config.I2C_DATA,
+                  oled_height=app_config.OLED_HEIGHT, oled_width=app_config.OLED_WIDTH, rotary_btn_pin=app_config.R_BTN_PIN,
+                 rotary_clock_pin=app_config.R_CL_PIN, rotary_data_pin=app_config.R_DT_PIN)
+
+# Intiate items for OLED screen menu
 plant_select = Selection(display, 'CHOOSE PLANT', ['Scoll to Select', 'Press to Confirm'], [
                          'lettuce', 'tomato', 'basil'], 'plant_type')
 
 garden_wiz = Wizard(display, [plant_select], lambda: print(display.menu_data.pop(
     'plant_type') if 'plant_type' in display.menu_data else print("Key not found")))
 
-setup_menu = Menu(display, [('SETUP GARDEN', ['192.168.5.97', ':5000/']), ('CHOOSE PLANT', ['Push Knob', 'to select plant'], garden_wiz), ('TEMPERATURE', display.temperature), ('PRESSURE', display.pressure), ('HUMIDITY', display.humidity),
-                ('PICO TEMP', display.internal_temp), ('WATER TEMP', display.thermistor_temp), ('', '')])
+# setup_menu = Menu(display, [('SETUP GARDEN', ['192.168.5.97', ':5000/']), ('CHOOSE PLANT', ['Push Knob', 'to select plant'], garden_wiz), ('TEMPERATURE', display.temperature), ('PRESSURE', display.pressure), ('HUMIDITY', display.humidity),
+#                 ('PICO TEMP', display.internal_temp), ('WATER TEMP', display.thermistor_temp), ('', '')])
+# 
+# display.set_current(setup_menu)
 
-display.set_current(setup_menu)
 
 
 
+# Creating an instance of the MQTT class for communication with the MQTT broker
+mqtt = MQTT(garden, mqtt_client, topic_pub=b'GardenData')
+
+# Creating an instance of the Microdot web server
 app = Microdot()
 Response.default_content_type = 'text/html'
 
+# Initialize the camera
+cam = Camera(SPI(0,sck=Pin(app_config.CAM_SCK), miso=Pin(app_config.CAM_MISO), mosi=Pin(app_config.CAM_MOSI)), Pin(app_config.CAM_CS, Pin.OUT))
 
-cam = Camera(SPI(0,sck=Pin(18), miso=Pin(16), mosi=Pin(19)), Pin(17, Pin.OUT))
-
-
-onboard_LED = Pin(25, Pin.OUT)
+# Initializing onboard LED pin
+onboard_LED = Pin(25, Pin.OUT) #Fixed Internal Pin Number
 sleep_ms(1000)
 
-
+# Setting up routes for the Microdot web server
 setup_routes(app, garden)
 
+# Creating a selection menu for resetting the
 reset_select = Selection(display, 'RESET GARDEN', [
                          'Would you like', 'to reset?'], ['YES', 'NO'], 'reset')
 
+# Creating a wizard for handling the reset selection
 reset_wiz = Wizard(display, [reset_select], lambda: print(display.menu_data.pop(
     'reset', False) if 'reset' in display.menu_data else print("Key not found")))
 
-main_menu = Menu(display, [('', ''), ('GARDEN INFO', display.garden_info), ('TEMPERATURE', display.temperature), ('BAROMETER', display.pressure), ('HUMIDITY', display.humidity),
-            ('CPUO TEMP', display.internal_temp), ('WATER TEMP', display.thermistor_temp), ('RESET', ['Press Button', 'To Reset', 'Garden'], reset_wiz), ('', '')])
+# Creating the main menu for display
+main_menu = Menu(display, [('', ''), ('SETUP GARDEN', ['192.168.5.97', ':5000/']), ('GARDEN INFO', display.garden_info),('CHOOSE PLANT', ['Push Knob', 'to select plant'], garden_wiz), ('TEMPERATURE', display.temperature), ('BAROMETER', display.pressure), ('HUMIDITY', display.humidity),
+            ('CPU TEMP', display.internal_temp), ('WATER TEMP', display.thermistor_temp), ('RESET', ['Press Button', 'To Reset', 'Garden'], reset_wiz), ('', '')])
 
+# Setting the main menu as the initial display
 display.set_current(main_menu)
 
 
+# Asynchronous tasks
+
 async def monitor_rotary_knob(display):
+    # Monitor rotary knob and button
     val_old = display.rotary_knob.value()
     but_old = display.rotary_button.value()
     while True:
         val_new = display.rotary_knob.value()
         but_new = display.rotary_button.value()
         if but_old != but_new:
+            # Button press detected
             but_old = but_new
             print("button =", but_new)
             display.current.on_click()
         if val_old != val_new:
+            # Rotary knob rotation detected
             if val_old < val_new:
                 value=1
             elif val_new < val_old:
@@ -79,22 +103,26 @@ async def monitor_rotary_knob(display):
 
 
 async def mqtt_report(mqtt):
+    # Periodically report sensor data to MQTT broker
     while True:
         if garden.plant_type is None:
-            await asyncio.sleep(3600)
+            await asyncio.sleep(3600) # Sleep for an hour if no plant is selected
         print("MQTT Reporting")
         try:
+            # Post sensor data to MQTT topics
             mqtt.post('temperatureF', mqtt.garden.temp)
             mqtt.post('humidity', mqtt.garden.hum)
             mqtt.post('barometer', mqtt.garden.pres)
             mqtt.post('pico_tempF', mqtt.garden.temp_internal)
             mqtt.post('therm_tempF', mqtt.garden.temp_therm)
+            mqtt.post('photo_resistor', mqtt.garden.photoresistor_percent())
         except Exception as error:
-            print("An error occured:", error)
-        await asyncio.sleep(3600)
+            print("An mqtt post error occured:", error)
+        await asyncio.sleep(3600) # Sleep for an hour
 
 
 async def run_waterpump(garden):
+    # Run water pump based on garden configuration
     while True:
         if garden.plant_type is not None:
             garden.waterpump.on()
@@ -112,6 +140,7 @@ async def run_waterpump(garden):
 
 
 async def run_airpump(garden):
+    # Run air pump based on garden configuration
     while True:
         if garden.plant_type is not None:
             duration = garden.data['airpump_duration']
@@ -125,95 +154,154 @@ async def run_airpump(garden):
         else:
             await asyncio.sleep(10)
             
-
+            
 async def heartbeat(garden):
+    # Toggle the attached LED as a heartbeat indicator
     while True:
         garden.led.on()
         await asyncio.sleep(1)
         garden.led.off()
         await asyncio.sleep(1)
             
-
+            
 def encoded_file_generator(filename, chunk_size, finish_marker='END'):
+    # Generator function to encode a file in chunks and yield as base64
+    print("encoded_file_generator")
     with open(filename, 'rb') as file:
         while True:
             chunk = file.read(chunk_size)
             
             if not chunk:
+                # Reached end of file, yield finish marker
                 yield finish_marker
                 break
-
             
             yield ubinascii.b2a_base64(chunk)
 
 
 def gardenpic_filename():
-    print(type(utime.time()))
-    return 'garden_pic-' +str(utime.time())
+    # Generate a filename for the garden picture
+    return f'{app_config.CLOUD_IMAGE_PREFIX}-time'
            
  
-def send_pic_mqtt(mqtt, filename, chunk_size=1536):
+def send_pic_mqtt(mqtt, filename, chunk_size=app_config.MQTT_CHUNK_SIZE):
+    # Send garden picture to MQTT in chunks
     gc.collect()
+    print("in send_pic_mqtt")
     pic_generator = encoded_file_generator(filename, chunk_size)
+    # image_generator = cam.generate_image_chunks()
+    print(pic_generator)
+    print(type(pic_generator))
     chunk_counter = 0
     filename = gardenpic_filename()
+    print(filename)
     gc.collect()
     for piece in pic_generator:
         print('send as mqtt message')
-        message = ujson.dumps({ 'machine_id': MACHINE_ID,
-                                'label' : 'garden_pic',
+        gc.collect()
+        print("Free MemoryGEN:", gc.mem_free())
+        message = ujson.dumps({ 'machine_id': app_config.MACHINE_ID,
+                                'label' : app_config.CLOUD_IMAGE_PREFIX,
                                 'filename': filename,
                                 'count' : chunk_counter,
                                 'data': piece })
-        print(message)
+        gc.collect()
+        print("Free MemoryGEN:", gc.mem_free())
         try:
             mqtt.mqtt_client.publish(mqtt.topic_pub, message)
+            print(message)
         except Exception as e:
             print(e)
         chunk_counter += 1
-        utime.sleep(1)
-    try:
-        print("removing picture")
-        os.remove('image.jpg')
-    except Exception as e:
-        print("pic removal failed")
-        print(e)
-
+        time.sleep(1)
+    
 
 async def take_garden_pic(mqtt, garden, cam):
+    # Capture and send garden picture periodically
     while True:
+        await asyncio.sleep(20)
+        try:
+            print("removing picture")
+            os.remove(app_config.IMAGE_FILE)
+        except Exception as e:
+            print("pic removal failed")
+            print(e)    
         if garden.plant_type is not None:
             gc.collect()
+            print("Free Memory7:", gc.mem_free())
             onboard_LED.on()
             cam.capture_jpg()
             gc.collect()
+            print("Free Memory8:", gc.mem_free())
             print("captured")
             sleep_ms(2000)
-            cam.saveJPG('image.jpg')
+            #cam.saveJPG(app_config.IMAGE_FILE)
             print("saved")
             onboard_LED.off()
-            sleep_ms(2000)
-            send_pic_mqtt(mqtt, 'image.jpg')
+            gc.collect()
+            chunk_counter = 0
+            print("Free Memory9:", gc.mem_free())
+            for jpg_chunk_hex in cam.generateJPG(1024):
+                #print(f'binary: {jpg_chunk_hex}')
+                jpeg_chunk_base64 = ubinascii.b2a_base64(jpg_chunk_hex)
+                #print(f'base64: {jpeg_chunk_base64}')
+                message = ujson.dumps({ 'machine_id': app_config.MACHINE_ID,
+                                'label' : app_config.CLOUD_IMAGE_PREFIX,
+                                'filename': 'test',
+                                'count' : chunk_counter,
+                                'data': jpeg_chunk_base64})
+                chunk_counter += 1
+                gc.collect()
+                print("Free MemoryGEN:", gc.mem_free())
+                try:
+                    mqtt.mqtt_client.publish(mqtt.topic_pub, message)
+                    print(message)
+                except Exception as e:
+                    print(e)
+                await asyncio.sleep(.1)
+            #send_pic_mqtt(mqtt, app_config.IMAGE_FILE)
             await asyncio.sleep(3600)
         else:
             await asyncio.sleep(60)
-    
+
+
+async def heater_control(garden):
+    # Control the garden heater based on temperature settings
+    garden.heater.duty_u16(0)
+    target_temp = 68 #config.garden.temp
+    wdt = WDT(timeout=8000)  #watchdog timer set for 8 seconds
+    while True:
+        wdt.feed() #watchdog timer reloads
+        desired_temp = int(garden.data['water_temp'])
+        print(f'desired_temp: {desired_temp}')
+        current_temp = float(garden.thermistor_temp()[1][:-2])
+        print(f'current_temp: {current_temp}') 
+        temp_dif = current_temp - desired_temp
+        if temp_dif < 0:
+            garden.heater.duty_u16(32768) #  duty cycle = duty_u16/65535
+        else:
+            garden.heater.duty_u16(0)
+        wdt.feed()    
+        await asyncio.sleep(3)    
+        
 
 async def microdot_server():
+    # Start the Microdot web server
     await app.start_server(port=5000, debug=True)
-
+    
 
 async def main():
+    # Main loop to run all asynchronous tasks
     while True:
         microdot_task = asyncio.create_task(microdot_server())
         waterpump_task = asyncio.create_task(run_waterpump(garden))
         airpump_task = asyncio.create_task(run_airpump(garden))
         mqtt_task = asyncio.create_task(mqtt_report(mqtt))
-        # mqtt_listen_task = asyncio.create_task(mqtt_listen(mqtt))
         knob_task = asyncio.create_task(monitor_rotary_knob(display))
         camera_task = asyncio.create_task(take_garden_pic(mqtt, garden, cam))
+        heater_task = asyncio.create_task(heater_control(garden))
         heartbeat_task = asyncio.create_task(heartbeat(garden))
-        await asyncio.gather(microdot_task, waterpump_task, mqtt_task, airpump_task, knob_task, heartbeat_task)
+        await asyncio.gather(microdot_task, waterpump_task, mqtt_task, airpump_task, knob_task, heartbeat_task, heater_task)
         await asyncio.sleep(10)
 
 
